9a4801694272b759a4f97e4b70c834657df88410
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-03-10 17:07:04 +0500

    delete column

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -29,7 +29,6 @@ const Users = () => {
                             <th scope='col'>Профессия</th>
                             <th scope='col'>Встретился,раз</th>
                             <th scope='col'>Оценка</th>
-                            <th scope='col'></th>
                         </tr>
                     </thead>
                     <tbody>
