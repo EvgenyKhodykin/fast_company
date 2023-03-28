aa8d1c1b0848c68e812e47526c25b407c34df6c5
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-03-28 19:09:13 +0500

    GroupList Two Types

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -2,32 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function GroupList({ items, valueProperty, contentProperty, onItemSelect, selectedItem }) {
-    const renderGroupList = data => {
-        if (Array.isArray(data)) {
-            return items.map(item => (
+    if (Array.isArray(items)) {
+        return (
+            <ul className='list-group'>
+                {items.map(item => (
+                    <li
+                        key={item[valueProperty]}
+                        className={'list-group-item' + (item === selectedItem ? ' active' : '')}
+                        onClick={() => onItemSelect(item)}
+                        role='button'
+                    >
+                        {item[contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    return (
+        <ul className='list-group'>
+            {Object.keys(items).map(item => (
                 <li
-                    key={item[valueProperty]}
-                    className={'list-group-item' + (item === selectedItem ? ' active' : '')}
-                    onClick={() => onItemSelect(item)}
+                    key={items[item][valueProperty]}
+                    className={'list-group-item' + (items[item] === selectedItem ? ' active' : '')}
+                    onClick={() => onItemSelect(items[item])}
                     role='button'
                 >
-                    {item[contentProperty]}
+                    {items[item][contentProperty]}
                 </li>
-            ))
-        }
-        return Object.keys(items).map(item => (
-            <li
-                key={items[item][valueProperty]}
-                className={'list-group-item' + (items[item] === selectedItem ? ' active' : '')}
-                onClick={() => onItemSelect(items[item])}
-                role='button'
-            >
-                {items[item][contentProperty]}
-            </li>
-        ))
-    }
-
-    return <ul className='list-group'>{renderGroupList(items)}</ul>
+            ))}
+        </ul>
+    )
 }
 
 GroupList.defaultProps = {
