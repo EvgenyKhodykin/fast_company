745fece0bef76d5fc42a6a99be2de734446d9308
Evgeny Khodykine <evgeny.khodykin@yandex.ru>
2023-04-04 14:12:59 +0500

    current result

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Pagination from './Pagination'
-import User from './User'
 import paginate from '../utils/paginate'
 import PropTypes from 'prop-types'
 import GroupList from './GroupList'
 import API from '../api'
 import SearchStatus from './SearchStatus'
+import UserTable from './UsersTable'
 
 function Users({ users, ...rest }) {
     const [currentPage, setCurrentPage] = useState(1)
@@ -30,11 +30,17 @@ function Users({ users, ...rest }) {
         setCurrentPage(pageIndex)
     }
 
+    const handleSort = item => {
+        console.log(item)
+    }
+
     const filteredUsers = selectedProf
         ? users.filter(user => user.profession._id === selectedProf._id)
         : users
 
     const count = filteredUsers.length
+
+    const sortedUsers = ''
 
     const userCrop = paginate(filteredUsers, currentPage, pageSize)
 
@@ -59,25 +65,7 @@ function Users({ users, ...rest }) {
 
             <div className='d-flex flex-column'>
                 <SearchStatus length={count} />
-                {count > 0 && (
-                    <table className='table  m-2'>
-                        <thead className='border-dark'>
-                            <tr>
-                                <th scope='col'>Имя</th>
-                                <th scope='col'>Качества</th>
-                                <th scope='col'>Профессия</th>
-                                <th scope='col'>Встретился,раз</th>
-                                <th scope='col'>Оценка</th>
-                                <th scope='col'>Избранное</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userCrop.map(user => (
-                                <User key={user._id} {...rest} {...user} />
-                            ))}
-                        </tbody>
-                    </table>
-                )}
+                {count > 0 && <UserTable users={userCrop} onSort={handleSort} {...rest} />}
                 <div className='d-flex justify-content-center'>
                     <Pagination
                         currentPage={currentPage}
