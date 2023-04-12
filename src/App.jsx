93581636ff6539d6e3751c6a0aa1f315a92da17f
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-04-12 10:47:12 +0500

    current result

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { React } from 'react'
 import { Route } from 'react-router-dom/cjs/react-router-dom.min'
-import Users from './components/Users'
 import NavBar from './components/NavBar'
 import Main from './layouts/Main'
 import Login from './layouts/Login'
+import Users from './components/Users'
 
 function App() {
     return (
         <>
             <NavBar />
-            <Route path='/main' component={Main} />
-            <Route path='/login' component={Login} />
-            <Route path='/users' component={Users} />
+            <Route exact path='/main' component={Main} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/users' component={Users} />
         </>
     )
 }
