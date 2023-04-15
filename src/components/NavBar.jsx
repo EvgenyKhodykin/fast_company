0b0696bf62207fc9ab613a2fe1bc75e9a0164499
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-04-15 13:41:54 +0500

    current routs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,23 +3,19 @@ import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 
 function NavBar() {
     return (
-        <ul className='nav nav-pills m-2'>
-            <li className='nav-item'>
-                <NavLink className='nav-link' aria-current='page' to='/' exact>
-                    Main
-                </NavLink>
-            </li>
-            <li className='nav-item'>
-                <NavLink className='nav-link' to='/login'>
-                    Login
-                </NavLink>
-            </li>
-            <li className='nav-item'>
-                <NavLink className='nav-link' to='/users'>
-                    Users
-                </NavLink>
-            </li>
-        </ul>
+        <nav className='nav nav-pills m-2'>
+            <NavLink className='nav-link' aria-current='page' to='/' exact>
+                Main
+            </NavLink>
+
+            <NavLink className='nav-link' to='/login'>
+                Login
+            </NavLink>
+
+            <NavLink className='nav-link' to='/users'>
+                Users
+            </NavLink>
+        </nav>
     )
 }
 
